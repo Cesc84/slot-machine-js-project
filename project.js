45064a7ace14ac23c98bc2055ca765087175581a
Cesc <f.daloe@alice.it>
2024-02-29 21:20:26 +0100

    spin the slot

--- project.js
+++ project.js
@@ -8,6 +8,24 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+  "A": 2,
+  "B": 4,
+  "C": 6,
+  "D": 8
+}
+
+const SYMBOLS_VALUE = {
+  "A": 5,
+  "B": 4,
+  "C": 3,
+  "D": 2
+}
+
+
 // 1. Deposit some money
 const deposit = () => {
   while (true) {
@@ -50,6 +68,42 @@ const getBet = (balance, lines) => {
   }
 }
 
+// 4. Spin the slot machine
+const spin = () => {
+  const symbols = [];
+  // a for loop to loop trough all the different entries inside the SYMBOL_COUNT object
+  for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+    // for loop to add however many symbols (A: 2, B: 4, C: 6, D: 8) into the symbols array.
+    for (let i = 0; i < count; i++) {
+      symbols.push(symbol);
+    }
+  }
+  // Each one of this array is going to rapresent a column inside the slot machine.
+  const reels = [];
+  // for every single reel(column of the slot machine) we have,
+  // we need to generate what's inside of it.
+  for (let i = 0; i < COLS; i++) {
+    reels.push([]);
+    // generate the available symbols
+    const reelSymbols = [...symbols];
+    // for every single array generate the number of rows and the number of elements
+    // that needs to go inside of that array or inside of each reel.
+    for (let j = 0; j < ROWS; j++) {
+      // generate a random number between 0 and wathever the length of our reel is - 1
+      const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+      // and then use that to select the element or the symbol from our reel symbols.
+      const selectedSymbols = reelSymbols[randomIndex];
+      // pushing into the interior array the selected symbol.
+      reels[i].push(selectedSymbols);
+      // remove the symbol cos we can't selected it again while we generate this reel.
+      reelSymbols.splice(randomIndex, 1);
+    }
+  }
+  return reels;
+};
+
+const reels = spin();
+console.log(reels);
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
